0a6042cced4fd2f71f8ef8943437648392f9f57d
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-09-30 20:56:37 -0300

    Funcionalidade de adicionar ao carrinho implementada

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Register from './components/register/Register';
-import Servicos from './components/Servicos/Servicos';
-import Carrinho from './components/Carrinho/Carrinho';
+import {Servicos} from './components/Servicos/Servicos';
+import Carrinho from './components/Servicos/Carrinho/Carrinho';
 
 
 export default class App extends React.Component {
@@ -22,7 +22,7 @@ export default class App extends React.Component {
 				case 'home':
 					return <Home changePage={this.changePage} />
 				case 'carrinho':
-					return <Carrinho changePage={this.changePage}/>
+					return <Carrinho servicesInCart={this.state.servicesInCart} changePage={this.changePage}/>
 				case 'servicos':
 					return <Servicos />
 				case 'register':
