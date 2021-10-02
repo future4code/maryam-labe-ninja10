256d17ff4a7ffbcf3ff4b2d0629bd5b23fa74e9b
Leonardo Souza Gomes <leo.sgomes60@gmail.com>
2021-10-01 21:55:06 -0300

    Carrinho estilizado

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,70 +1,146 @@
 import React from 'react';
 import styled from 'styled-components';
 import { servicesInCart } from '../Servicos/Servicos'
+import lixeira from "../../imgs/lixeira1.png"
 
-const CarrinhoContainer = styled.div`
-    border-radius: 10px;
-    background-color: #FF9933;
-    padding: 0px 16px 16px;
-    margin: 30px;
-    width: 300px;
-    height: 200px;
-    text-align: center;
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-    font-family: Graphik-Medium, Graphik-Regular, "Gotham SSm A", "Gotham SSm B", "Helvetica Neue", Helvetica, Arial, sans-serif;
-`
-const BotaoCarrinho = styled.div`
+// Background Carrinho
+
+const BackgroundSite = styled.div`
+    background-color: #F5F6FA;
+    margin: 0;
     display: flex;
     flex-direction: column;
-    margin-left: 10px;
-    margin-top: 20px;
-    
-    button {
-        margin-bottom: 10px;
-    }
-
+    align-items: center;
+    height: 100%;
+`
+const ContainerPrincipal = styled.div`
+    display: flex;
 `
 
-const CardCarrinhoContainer = styled.div`
-    border-radius: 5px;
-    background-color: ghostwhite;
-    padding: 0px 16px 16px;
-    margin: 1px;
-    width: 200px;
-    height: 150px;
-    text-align: center;
-`
-const VoltarContainer = styled.div`
-    margin-left: 100px;
 
+// Container Carrinho
 
-`
 const ServicesContainer = styled.div`
     display: flex;
     flex-direction: column;
+    margin: 0 auto;
 `
+
+const CarrinhoContainer = styled.div`
+    display: flex;
+    background-color: #FF9933;
+    box-shadow: 4px 6px 3px #727D71;
+
+    border-radius: 10px;
+    padding: 0px 16px 16px;
+    margin-top: 60px;
+
+    width: 550px;
+    height: 120px;
+    text-align: center;
+    align-items: center;
+    font-family: Graphik-Medium, Graphik-Regular, "Gotham SSm A", "Gotham SSm B", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: 600;
+    font-style: small;
+`
+
+const CardCarrinhoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    text-align: center;
+
+    background-color: #FFFFFF;
+    box-shadow: 4px 5px 3px #a25922;
+	border: 1px solid gray;
+    width: 400px;
+    height: 80px;
+    margin: 0 auto;
+    margin-top: 15px;
+    border-radius: 10px;
+
+`
+
+const BotaoLixeira = styled.button`
+    box-shadow: 3px 5px 3px #a25922;
+    border: hidden;
+    border-radius: 5px;
+    
+    background-color: red;
+    cursor: pointer;
+    
+    font-weight: 800;
+    margin-top: 15px;
+    margin-right: 15px;
+    img{
+        margin-top: 5px;
+    }
+`
+
+
+// Soma Container
 
 const SomaContainer = styled.div`
-    justify-content: left;
+	border: 1px solid gray;
     border-radius: 30px;
-    background-color: #FF9933;
-    padding: 0px 16px 16px;
-    margin: 100px;
-    width: 300px;
-    height: 200px;
-    text-align: center;
-    border-radius: 10px;
-`
-const ContainerCima = styled.div`
-    display: flex;  
+    background-color: white;
+    -webkit-box-shadow: 4px 6px 3px #727D71;
 
+    width: 300px;
+    height: 150px;
+    text-align: center;
+    justify-content: space-evenly;
+
+    display: flex;
+    flex-direction: column;
+    margin-top: 100px;
+    margin-left: 100px;
+    color: #FF9933;
+
+    h3{
+        color:black;
+    }
 `
-const FundoCarrinho = styled.div`
-    background-color: #F5F6FA;
+
+const BotaoCompra = styled.button`
+    margin-bottom: 10px;
+    box-shadow: 3px 5px 3px #727D71;
+    border: none;
+    border-radius: 20px;
+    background-color: #FF9933;
+    color: white;
+    cursor: pointer;
+    border: hidden;
+    font-weight: bold;
+    font-weight: 800;
+    width: 200px;
+    margin: 0 auto;
 `
+
+const VoltarContainer = styled.div`
+    margin-right: 400px;
+    margin-top: 60px;
+`
+
+const BotaoVoltar = styled.button`
+    margin: 0 auto;
+    margin-bottom: 60px;
+    width: 200px;
+    height: 30px;
+    
+    box-shadow: 3px 5px 3px #727D71;
+	border: none;
+    border-radius: 20px;
+    background-color: #FF9933;
+    color: white;
+    cursor: pointer;
+    border: hidden;
+    font-weight: bold;
+    font-weight: 800;
+`
+
+
+
 export default class Carrinho extends React.Component {
     state = {
         cart: servicesInCart
@@ -111,33 +187,37 @@ export default class Carrinho extends React.Component {
 
     render() {
         return (
-            <FundoCarrinho> 
-                <ContainerCima>
+            <BackgroundSite> 
+                
+                <ContainerPrincipal>
+                    
                     <ServicesContainer>
                         {this.state.cart.map((servico) => {
                             return (
                                 <CarrinhoContainer key={servico.id}>
                                     <CardCarrinhoContainer>
-                                        <h2>{servico.title}</h2><li><b>Preço:<b>
-                                        </b> R$ {servico.price}</b></li>
+                                        <h3>{servico.title}</h3>
+                                        <h3>Preço: R$ {servico.price}</h3>
                                     </CardCarrinhoContainer>
-                                    <BotaoCarrinho>
-                                        <button onClick={() => this.removeServiceCart(servico.id)}> Remover</button>
-                                    </BotaoCarrinho>
-                                </CarrinhoContainer>
+                                        <BotaoLixeira onClick={() => this.removeServiceCart(servico.id)}><img src={lixeira} alt="lixeira imagem" /></BotaoLixeira>
+                                    </CarrinhoContainer>
                             )
                         })}
                     </ServicesContainer>
+                    
                     <SomaContainer>
-                        <h2>Valor Total</h2><b>Preço: <b>
-                        </b> R$ {this.totalCart()}</b><br></br>
-                        <button onClick={this.CompletedCart}> Finalizar Compra</button><br></br>
+                        <h2>Total de compras</h2>
+                        <h3>Preço: R$ {this.totalCart()}</h3>
+                        <BotaoCompra onClick={this.CompletedCart}> Finalizar Compra</BotaoCompra>
                     </SomaContainer>
-                </ContainerCima>
+                
+                </ContainerPrincipal>
+                
                 <VoltarContainer>
-                    <button onClick={() => this.props.changePage("servicos")}>Voltar para Lista</button>
+                    <BotaoVoltar onClick={() => this.props.changePage("servicos")}>Voltar para Lista</BotaoVoltar>
                 </VoltarContainer>
-            </FundoCarrinho>    
+            
+            </BackgroundSite>    
         )
     }
 }
